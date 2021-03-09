a7e16d02f6e69ecb6f721e06be438541f6e1c2b5
Denisa Calin <denisa.calin@global-technical.com>
2021-03-09 15:35:07 +0200

    Third phase

--- Week_3 - Clean Code Refactoring/src/ro/ase/acs/cta/g1094/refactor/phase1/Product.java
+++ Week_3 - Clean Code Refactoring/src/ro/ase/acs/cta/g1094/refactor/phase1/Product.java
@@ -1,12 +1,21 @@
 package ro.ase.acs.cta.g1094.refactor.phase1;
 
+import ro.ase.acs.cta.g1094.refactor.phase1.exception.InvalidAgeException;
+import ro.ase.acs.cta.g1094.refactor.phase1.exception.InvalidPriceException;
+
 public class Product {
 	
 	public static final int MAX_AGE_ACCOUNT = 10;
 	public static final float MAX_FIDELITY_DISCOUNT = (float) 0.15; // or 0.15f
 
-	public float computePriceWithDiscount(ProductType productType, float price, int accountAge)
+	public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceException, InvalidAgeException
 	  {
+		if (price <= 0) {
+			throw new InvalidPriceException();
+		}
+		if (accountAge < 0) {
+			throw new InvalidAgeException();
+		}
 	    float finalPrice = 0;
 	    float fidelityDiscount = (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float) accountAge / 100; 
 	    
---
+++ Week_3 - Clean Code Refactoring/src/ro/ase/acs/cta/g1094/refactor/phase1/exception/InvalidAgeException.java
@@ -0,0 +1,5 @@
+package ro.ase.acs.cta.g1094.refactor.phase1.exception;
+
+public class InvalidAgeException extends Exception {
+
+}
