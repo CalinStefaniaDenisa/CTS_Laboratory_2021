e6a837a4fb30630c5ba951a79d6eed6423102dc8
Denisa Calin <denisa.calin@global-technical.com>
2021-03-30 15:30:24 +0300

    Prototype implementation

---
+++ Week_6 - Builder and Prototype/src/ro/ase/csie/cts/g1094/dp/prototype/TestPrototype.java
@@ -0,0 +1,21 @@
+package ro.ase.csie.cts.g1094.dp.prototype;
+
+public class TestPrototype {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+
+		// inefficient
+		Tree3DModel tree1 = new Tree3DModel("green", 100);
+		Tree3DModel tree2 = new Tree3DModel("green", 100);
+		
+		// shalow-copy
+		Tree3DModel anotherTree = tree1;
+		
+		// build multiple trees using clone
+		// deep-copy
+		Tree3DModel tree3 = (Tree3DModel) tree1.clone();
+		Tree3DModel tree4 = (Tree3DModel) tree1.clone();
+		Tree3DModel tree5 = (Tree3DModel) tree1.clone();
+	}
+
+}
