9b234720272e62b78c0490fb064d61c1cdd8b07e
Denisa Calin <denisa.calin@global-technical.com>
2021-04-13 15:39:42 +0300

    Adapter and Decorator implementation

--- Week_8 - Facade, Decorator, Adapter/src/ro/ase/csie/cts/g1094/dp/adapter/test/TestAdapter.java
+++ Week_8 - Facade, Decorator, Adapter/src/ro/ase/csie/cts/g1094/dp/adapter/test/TestAdapter.java
@@ -1,16 +1,40 @@
 package ro.ase.csie.cts.g1094.dp.adapter.test;
 
+import java.util.ArrayList;
+
+import ro.ase.csie.cts.g1094.dp.adapter.ACMESuperHero;
+import ro.ase.csie.cts.g1094.dp.adapter.Disney2AcmeAdapter;
 import ro.ase.csie.cts.g1094.dp.adapter.FantasyHero;
+import ro.ase.csie.cts.g1094.dp.adapter.disney.DisneyActions;
+import ro.ase.csie.cts.g1094.dp.adapter.disney.DonaldDuck;
 
 public class TestAdapter {
 
 	public static void main(String[] args) {
 
-		FantasyHero blueDragon = new FantasyHero("Blue dragon", 1000, true);
+		ACMESuperHero blueDragon = new FantasyHero("Blue dragon", 1000, true);
 		blueDragon.move();
 		blueDragon.takeAHit(500);
 		blueDragon.heal(250);
+		
+		DisneyActions donald = new DonaldDuck(100);
+		donald.changeLocation(100, 150);
+		donald.isWounded(50);
+		donald.restorePower(25);
 
+		System.out.println("......................After adapter......................");
+
+		ArrayList<ACMESuperHero> heroes = new ArrayList<>();
+		heroes.add(blueDragon);
+		Disney2AcmeAdapter donaldAdapter = new Disney2AcmeAdapter(donald);
+		heroes.add(donaldAdapter);
+		
+
+		for(ACMESuperHero hero: heroes) {
+			hero.move();
+			hero.takeAHit(50);
+			hero.heal(40);
+		}
 	}
 
 }
